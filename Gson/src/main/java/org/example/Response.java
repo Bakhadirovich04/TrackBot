93185351732d4d7b7f8bf5214692bd67dcb89f48
Bakhadirovich04 <choriyevhusan74@gmail.com>
2025-03-04 17:45:59 +0500

    Librrays added

---
+++ Gson/src/main/java/org/example/Response.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import lombok.Data;
+
+@Data
+public class Response{
+	private String CcyNm_EN;
+	private String CcyNm_UZC;
+	private Double Diff;
+	private Double Rate;
+	private String Ccy;
+	private String CcyNm_RU;
+	private int id;
+	private String CcyNm_UZ;
+	private String Code;
+	private Integer Nominal;
+	private String Date;
+
+	}
